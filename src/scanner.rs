3bd7663dd119df90e6c0c72a5e2c0bf63808ac0b
Ifenna Awagu <jawagu@gmail.com>
2019-10-07 13:06:05 +0100

    Refactor parser to handle line numbers better

--- src/scanner.rs
+++ src/scanner.rs
@@ -46,6 +46,9 @@ pub enum Token {
     While,
 
     Error(String),
+
+    Comment,
+    Whitespace,
     EOF,
 }
 
@@ -76,6 +79,13 @@ pub struct Lexeme {
     pub position: Position,
 }
 
+fn is_whitespace(c: char) -> bool {
+    match c {
+        ' ' | '\r' | '\t' | '\n' => true,
+        _ => false,
+    }
+}
+
 #[derive(Debug)]
 pub enum ScanError {
     UnknownCharacter(Position, String),
@@ -90,25 +100,35 @@ pub struct Scanner<'a> {
 impl<'a> Scanner<'a> {
     pub fn new(text: &'a std::string::String) -> Scanner<'a> {
         Scanner {
-            source: itertools::multipeek(text.chars().into_iter()),
+            source: itertools::multipeek(text.chars()),
             current_string: String::new(),
             current_position: Position::reset(),
         }
     }
 
     pub fn scan_token(&mut self) -> Result<Lexeme, ScanError> {
+        self.current_string.clear();
         match self.advance() {
-            Some("(") => self.make_token(Token::LeftParen),
-            Some(")") => self.make_token(Token::RightParen),
-            Some("{") => self.make_token(Token::LeftBrace),
-            Some("}") => self.make_token(Token::RightBrace),
-            Some(";") => self.make_token(Token::SemiColon),
-            Some(",") => self.make_token(Token::Comma),
-            Some(".") => self.make_token(Token::Dot),
-            Some("-") => self.make_token(Token::Minus),
-            Some("+") => self.make_token(Token::Plus),
-            Some("/") => self.make_token(Token::Slash),
-            Some("*") => self.make_token(Token::Star),
+            Some('(') => self.make_token(Token::LeftParen),
+            Some(')') => self.make_token(Token::RightParen),
+            Some('{') => self.make_token(Token::LeftBrace),
+            Some('}') => self.make_token(Token::RightBrace),
+            Some(';') => self.make_token(Token::SemiColon),
+            Some(',') => self.make_token(Token::Comma),
+            Some('.') => self.make_token(Token::Dot),
+            Some('-') => self.make_token(Token::Minus),
+            Some('+') => self.make_token(Token::Plus),
+            Some('/') => {
+                if self.peek_match('/') {
+                    let token = self.make_token(Token::Comment);
+                    self.advance_until_newline();
+                    token
+                } else {
+                    self.make_token(Token::Slash)
+                }
+            }
+            Some('*') => self.make_token(Token::Star),
+            Some(c) if is_whitespace(c) => self.make_token(Token::Whitespace),
             None => self.make_token(Token::EOF),
             _ => Err(ScanError::UnknownCharacter(
                 self.current_position,
@@ -117,33 +137,30 @@ impl<'a> Scanner<'a> {
         }
     }
 
-    fn advance(&mut self) -> Option<&str> {
-        let mut character;
-        self.current_string = String::new();
+    fn advance(&mut self) -> Option<char> {
+        let character = self.source.next();
+        if let Some(ch) = character {
+            self.current_string.push(ch);
+            if ch == '\n' {
+                self.current_position.next_line();
+            } else {
+                self.current_position.increment_column();
+            }
+        }
+        character
+    }
+
+    fn peek_match(&mut self, ch: char) -> bool {
+        if self.source.peek() == Some(&ch) {
+            return true;
+        }
+        false
+    }
+
+    fn advance_until_newline(&mut self) {
         loop {
-            character = self.source.next();
-            match character {
-                Some('\t') | Some(' ') | Some('\r') => {
-                    self.current_position.increment_column();
-                    return Some(&self.current_string);
-                }
-                Some('\n') => {
-                    self.current_position.next_line();
-                    if self.current_string.len() > 0 {
-                        return Some(&self.current_string);
-                    }
-                }
-                Some('/') => {
-                    // if the next char is '/', this is a comment and we skip
-                    if Some(&'/') == self.source.peek() {
-                        self.source.next();
-                        while self.source.peek() != Some(&'\n') {
-                            self.source.next();
-                        }
-                    }
-                }
-                Some(ch) => self.current_string.push(ch),
-                None => return None,
+            if let Some('\n') = self.advance() {
+                break;
             }
         }
     }
